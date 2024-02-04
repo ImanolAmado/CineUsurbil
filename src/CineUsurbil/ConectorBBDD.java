625361e4790aa58c8287914d89c32559cfc91398
imanol <imanolamado@hotmail.com>
2024-02-04 14:40:38 +0100

    commit funcionaMALDITO

---
+++ src/CineUsurbil/ConectorBBDD.java
@@ -0,0 +1,37 @@
+package CineUsurbil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConectorBBDD {    
+           
+    private final String url = "jdbc:mysql://localhost:3306/cineusurbil";
+    private final String user = "test";
+    private final String pass = "pass"; 
+
+    public Peliculas[] pelicula;
+
+    public Peliculas[] Conectar (String sql) throws SQLException {
+        
+        Connection conn = DriverManager.getConnection(url, user, pass);
+       
+        PreparedStatement ps = conn.prepareStatement(sql);
+        
+        ResultSet rs= ps.executeQuery();
+
+        while (rs.next()){
+
+            
+            System.out.println(rs.getString(1));
+        }
+
+        rs.close();
+        ps.close();
+        conn.close();   
+
+        return pelicula;
+    }
+}
